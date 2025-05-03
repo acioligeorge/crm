c020687ea14c186716b10c82002bfdd633c29a9f
George Acioli <acioligeorge@gmail.com>
2025-05-03 20:55:32 +0100

    fix: corrigir caminho de importação para socketio_port

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'
-import { socketio_port } from '../../../../sites/common_site_config.json'
+import { socketio_port } from '../../sites/common_site_config.json'
 import { getCachedListResource } from 'frappe-ui/src/resources/listResource'
 import { getCachedResource } from 'frappe-ui/src/resources/resources'
 
